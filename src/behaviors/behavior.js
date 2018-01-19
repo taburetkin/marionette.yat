95a5e49ef0abf201bb6f36cef2c8fb31429e97a9
dimaver <dima.taburetkin@gmail.com>
2018-01-19 20:42:49 +0300

    remove comments

--- src/behaviors/behavior.js
+++ src/behaviors/behavior.js
@@ -6,26 +6,9 @@ import fns from '../functions/common/common';
 const BaseBehavior = mix(Mn.Behavior).with(GetOptionProperty);
 export default BaseBehavior.extend({
 	constructor(){
-
-		//this._viewInitialized = false;
-		// this.on('initialize',() => {
-		// 	if(!this._viewInitialized){
-		// 		this._viewInitialized = true;
-		// 		this.triggerMethod('view:initialize');
-		// 	}
-		// });		
 		this.on('before:render initialize', _.once(_.partial(this.triggerMethod, 'view:initialize')));
 		BaseBehavior.apply(this, arguments);
-		// this.listenTo(this.view, 'before:render',() => {
-		// 	if(!this._viewInitialized){
-		// 		this._viewInitialized = true;
-		// 		this.triggerMethod('view:initialize');
-		// 	}
-		// });
 	},
-	// getProperty(...args){
-	// 	return this.getOption(...args);
-	// },
 	getModel: function () {
 		return this.view.model;
 	},	
