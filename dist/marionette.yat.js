9fe9632bc9b75ba11bd7c580507785df1fe476be
dimaver <dima.taburetkin@gmail.com>
2018-01-19 20:47:37 +0300

    v0.0.19

--- dist/marionette.yat.js
+++ dist/marionette.yat.js
@@ -3,7 +3,7 @@
 * Marionette.Yat extension for Backbone.Marionette
 * Yet Another Toolkit
 * ----------------------------------
-* v0.0.18
+* v0.0.19
 *
 * Distributed under MIT license
 * author: dimtabu
@@ -20,7 +20,7 @@ Bb = Bb && Bb.hasOwnProperty('default') ? Bb['default'] : Bb;
 Mn = Mn && Mn.hasOwnProperty('default') ? Mn['default'] : Mn;
 _ = _ && _.hasOwnProperty('default') ? _['default'] : _;
 
-var version = "0.0.18";
+var version = "0.0.19";
 
 var getCompareABModel = function getCompareABModel(arg) {
 	if (arg instanceof Bb.Model) return arg;else if (arg instanceof Mn.View) return arg.model;else return;
